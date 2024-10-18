6d0319f21e0ffc724d474d5d06f7e49d456a0365
JucierG <jucier.gregorio05@gmail.com>
2024-10-18 16:06:01 -0300

    Footer progress

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,9 +4,8 @@ React
 
 export function Footer(){
     return(
-        <div className="bg-header text-center flex justify-end w-[230px] rounded-xl">
+        <div className="bg-header flex flex-col w-[230px] rounded-xl">
             <footer >
-                <h1>Jucier Gregório</h1>
                 <h1>Jucier Gregório</h1>
             </footer>
         </div>
