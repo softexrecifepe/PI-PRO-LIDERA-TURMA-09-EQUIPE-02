aca4d6ef48a9b2284e742d826d9af884d3d7cf34
JucierG <jucier.gregorio05@gmail.com>
2024-10-21 15:06:53 -0300

    new mission

--- src/app/components/registerForm.tsx
+++ src/app/components/registerForm.tsx
@@ -11,6 +11,7 @@ export function RegisterForm(){
                 <h1 className="text-3xl font-bold p-3">Realizar Cadastro</h1>
                 <form className="mx-12">
                     {/* // TROCAR TODOS OS "H1" POR <label></label> P USAR C INPUT*/}
+                    {/* Criar um compotente para o <input type="text" /> */}
                     <h1 className="absolute bg-box transform translate-x-[15%] translate-y-1/3 font-semibold px-2">Digite seu nome</h1>
                     <input type="text" className="rounded-3xl border border-black my-5 w-full p-2" name="" id="name"/>
                     <h1 className="absolute bg-box transform translate-x-[10%] translate-y-1/3 font-semibold px-2">Digite seu sobrenome</h1>
