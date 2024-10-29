7f669e7cb0ffc18808b040eeccd45e4d5b932499
Mateus Fernando <mateusfss368@gmail.com>
2024-10-29 19:35:41 +0000

    Changing the font of the header title to Roboto

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export function Header() {
     return(
-            <header className="bg-box p-0 h-32 flex gap-5 text-right w-[100%] m-0">
+            <header className="bg-box p-0 h-32 flex gap-5 text-right w-[100%] m-0 header-titulo">
                 <h1 className="my-auto ml-14 font-semibold pl-2 text-3xl">Pro Lidera Skills</h1>
                 <Link href="/" className="text-2xl ml-auto my-auto mr-14">Página inicial</Link>
                 <h1 className="text-2xl my-auto mr-14">Sobre o programa</h1>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Head from "next/head"; // Import Head component
 import "./globals.css";
 import { Header } from "./components/header";
 
@@ -26,9 +27,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${nunito.variable} ${roboto.variable} antialiased`}
-      >
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+        {/* Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className={`${nunito.variable} ${roboto.variable} antialiased`}>
         <Header />
         {children}
       </body>
