cdf7a1ad3b833bfe3d249e3f4892aa5d8ecefabe
Mateus Fernando <mateusfss368@gmail.com>
2024-10-30 15:53:30 +0000

    mudança de idioma principal html

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
