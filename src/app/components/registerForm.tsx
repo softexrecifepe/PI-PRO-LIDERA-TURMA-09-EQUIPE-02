0d373352baf73a78d1f7b65c5bff923c31c0bee9
Luana Calisto <luanavenancio060@gmail.com>
2024-11-03 13:06:57 -0300

    created inputText component

--- src/app/components/registerForm.tsx
+++ src/app/components/registerForm.tsx
@@ -1,22 +1,15 @@
 import React from "react"
 import { Button } from "./button";
+import { InputText } from "./textInput";
 
 export function RegisterForm(){
     return(
         <div className="bg-box w-96 mt-20 mx-auto p-3 text-center rounded-2xl">
                 <h1 className="text-3xl font-bold p-3">Realizar Cadastro</h1>
-                <form className="mx-12">
-                    {/* // TROCAR TODOS OS "H1" POR <label></label> P USAR C INPUT*/}
-                    {/* Criar um compotente para o <input type="text" /> */}
-                    <h1 className="absolute bg-box transform translate-x-[15%] translate-y-1/3 font-semibold px-2">Digite seu nome</h1>
-                    <input type="text" className="rounded-3xl border border-black my-5 w-full p-2" name="" id="name"/>
-                    <h1 className="absolute bg-box transform translate-x-[10%] translate-y-1/3 font-semibold px-2">Digite seu sobrenome</h1>
-                    <input type="text" className="rounded-3xl border border-black my-5 w-full p-2" name="" id="surname"/>
-                    <h1 className="absolute bg-box transform translate-x-[15%] translate-y-1/3 font-semibold px-2">Digite seu email</h1>
-                    <input type="text" className="rounded-3xl border border-black my-5 w-full p-2" name="" id="email" />
-                    <br></br>
+                    <InputText></InputText>
+                    <InputText></InputText>
+                    <InputText></InputText>
                     <Button></Button>
-                </form>
         </div>
     )
 }
