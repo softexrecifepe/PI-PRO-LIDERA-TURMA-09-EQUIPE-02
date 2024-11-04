d717e56f07c6c51695395b6ecacaa28280f2b47a
Luana Calisto <luanavenancio060@gmail.com>
2024-11-04 00:11:42 -0300

    button hover fixed

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 
 export function Button({page}: {page: string}, {disabled = false}) {
     return (
-        <button disabled={disabled} className={`rounded-xl bg-gradient-to-r from-button-orange to-button-green px-1 py-2 font-semibold shadow-md transition ease-in-out duration-300`}>
-           <Link href="/quests1-2" className='w-full h-full rounded-lg px-4 py-1 bg-box hover:bg-slate-200'>{page}</Link>
+        <button disabled={disabled} className={`rounded-xl bg-gradient-to-r from-button-orange to-button-green px-1 py-2 font-semibold shadow-md`}>
+           <Link href="/quests1-2" className='w-full h-full rounded-lg px-4 py-1 bg-box transition ease-in-out delay-75 hover:bg-slate-200 duration-300 '>{page}</Link>
         </button>
     )
 }
