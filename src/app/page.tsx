2b29116945d426eccd3d16c7e4c7539119ef81a3
Luana Calisto <luanavenancio060@gmail.com>
2024-11-03 21:49:52 -0300

    initial page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
+import mockup from './resources/leadershipMockup.png';
 import { RegisterForm } from "./components/registerForm";
+import { PageContent } from "./components/pageContent";
 
 export default function Home(){
  
   return (
     <div>
+      <PageContent titleText='Teste de avaliação de liderança' imageSrc={mockup.src}
+       textContent="Seja bem-vindo ao nosso teste de avaliação de liderança! Com apenas 18 questões, nosso teste tem como finalidade definir seu nível 
+       de liderança. Cadastre-se para começar a responder!"></PageContent>
       <RegisterForm></RegisterForm>
     </div>
   )
