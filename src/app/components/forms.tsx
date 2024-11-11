d22d3a35e16b99c498e03f5c8dd468907a5c35b3
Calistofolio <luanavenancio060@gmail.com>
2024-11-11 15:27:03 -0300

    feat: updated data.json and forms to get the json questions

--- src/app/components/forms.tsx
+++ src/app/components/forms.tsx
@@ -1,27 +1,41 @@
 import React from "react";
 import { MultipleChoiceQuestion } from "./MultipleChoiceQuestion";
+import Data from "../../../data.json"
 
 export function Forms() {
-    const questionData = {
-      question: 'Qual sua linguagem de programação favorita?',
-      options: [
-        { id: 1, label: 'JavaScript' },
-        { id: 2, label: 'Python' },
-        { id: 3, label: 'Java' },
-        { id: 4, label: 'C++' },
-      ],
-    };
+  return(
+    <div  className="bg-box w-[550px] mt-6 mx-auto p-3 text-center rounded-2xl">
+    {
+      Data.map(post => (
+        <div>
+        <p>Questão {post.id}</p>
+        <h2>{post.pergunta}</h2>
+        </div>
+      ))
+  }
+  </div>
+  )
+  
+    // const questionData = {
+    //   question: 'Qual sua linguagem de programação favorita?',
+    //   options: [
+    //     { id: 1, label: 'JavaScript' },
+    //     { id: 2, label: 'Python' },
+    //     { id: 3, label: 'Java' },
+    //     { id: 4, label: 'C++' },
+    //   ],
+    // };
 
     
-    const formInstances = Array.from({ length: 18 }, (_, index) => (
-      <div key={index}>
-        <form className="mx-auto">
-          <h1 className="my-4 font-bold text-xl">{`Pergunta ${index + 1}`}</h1>
-          <MultipleChoiceQuestion question={questionData.question} options={questionData.options} />
-        </form>
-      </div>
+    // const formInstances = Array.from({ length: 18 }, (_, index) => (
+    //   <div key={index}>
+    //     <form className="mx-auto">
+    //       <h1 className="my-4 font-bold text-xl">{`Pergunta ${index + 1}`}</h1>
+    //       <MultipleChoiceQuestion question={questionData.question} options={questionData.options} />
+    //     </form>
+    //   </div>
   
-    ));
+    // ));
   
-    return <div className="bg-box w-[550px] mt-6 mx-auto p-3 text-center rounded-2xl">{formInstances}</div>;
+    // return <div className="bg-box w-[550px] mt-6 mx-auto p-3 text-center rounded-2xl"></div>;
   }
