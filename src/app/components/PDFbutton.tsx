a34aa2bd2262d76051b3759826566a9595429ab9
Mateus Fernando <mateusfss368@gmail.com>
2024-11-26 22:05:41 +0000

    feat: new pdf button

--- src/app/components/PDFbutton.tsx
+++ src/app/components/PDFbutton.tsx
@@ -1,81 +1,161 @@
 import React from "react";
-import { jsPDF } from "jspdf";  // importa a biblioteca jsPDF para gerar o PDF
-import "jspdf-autotable";  // importa o plugin jsPDF AutoTable para criar tabelas no PDF
-import { Button } from "@/app/components/button";  // importa o componente Button
+import { Button } from "@/app/components/button";
+import Results from "../json/results.json";
+import { PDFDownloadLink, Document, Page, Text, View, StyleSheet, pdf } from "@react-pdf/renderer";
 
-// define o tipo das propriedades esperadas pelo componente PDFButton
-type PDFButtonProps = {
-  name: string;  // nome do usuário
-  surname: string;  // sobrenome do usuário
-  leadershipLevel: string;  // nível de liderança do usuário
-  score: number;  // pontuação do usuário
+// Função para obter o resultado com base na pontuação
+const getResultFromJSON = (score: number) => {
+  return (
+    Results.find(
+      ({ scoreRange }) =>
+        score >= (scoreRange[0] ?? -Infinity) && score <= (scoreRange[1] ?? Infinity)
+    ) || { result: "Pontuação Não Encontrada", text: "Houve um erro, por favor tente novamente." }
+  );
 };
 
-// função que define o componente PDFButton
-export const PDFButton: React.FC<PDFButtonProps> = ({ name, surname, leadershipLevel, score }) => {
-  // função para gerar o PDF quando o botão é clicado
-  const handlePrint = () => {
-    // criação de uma nova instância do jsPDF para gerar o documento
-    const doc = new jsPDF("portrait", "mm", "a4");
+type PDFButtonProps = {
+  score: number;
+};
 
-    // definindo as margens do documento
-    const margin = { top: 10, left: 10, bottom: 10, right: 10 };
+const PDFDocument = ({ score, name }: { score: number, name: string }) => {
+  const { result: leadershipLevel, text: feedbackText } = getResultFromJSON(score);
 
-    // definindo o tamanho e cor da fonte para o título
-    doc.setFontSize(22);
-    doc.setTextColor(41, 128, 185);
-    doc.text("Resultados do Teste Pro Lidera Skills", 105, margin.top + 10, { align: "center" });
+  const styles = StyleSheet.create({
+    page: {
+      padding: 15, // Aumento das margens para mais espaço
+      fontFamily: "Helvetica",
+      backgroundColor: "#fff",
+      minHeight: "100%",
+    },
+    title: {
+      fontSize: 20, // Fonte maior para o título
+      fontWeight: "bold",
+      color: "#2980b9",
+      textAlign: "center",
+      marginBottom: 13, // Ajustando a margem inferior do título
+    },
+    section: {
+      marginBottom: 0, // Aumento da margem entre as seções
+    },
+    text: {
+      fontSize: 12, // Tamanho maior para o texto
+      color: "#333",
+      lineHeight: 1.5, // Aumento do espaçamento entre as linhas
+      textAlign: "justify",
+      marginBottom: 5, // Maior margem inferior
+    },
+    table: {
+      display: "table",
+      width: "100%",
+      borderRadius: 8,
+      overflow: "hidden",
+      marginBottom: 10, // Mais espaço após a tabela
+    },
+    tableRow: {
+      flexDirection: "row",
+      padding: 8, // Aumento do padding nas linhas da tabela
+      borderBottom: "1px solid #ddd",
+    },
+    tableHeader: {
+      backgroundColor: "#2980b9",
+      color: "#fff",
+      fontSize: 12, // Tamanho de fonte maior para os cabeçalhos
+      fontWeight: "bold",
+    },
+    tableCell: {
+      width: "50%",
+      padding: 8, // Aumento do padding nas células
+      fontSize: 12, // Tamanho de fonte maior
+      color: "#333",
+      textAlign: "left",
+    },
+    feedbackSection: {
+      padding: 15, // Aumento do padding no feedback
+      backgroundColor: "#f4f4f9",
+      borderRadius: 8,
+      boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+      marginBottom: 10, // Maior espaço após o feedback
+    },
+    feedbackText: {
+      fontSize: 12, // Aumento da fonte do feedback
+      color: "#3c3c3c",
+      lineHeight: 1.5, // Maior espaçamento entre as linhas do feedback
+    },
+    feedbackTitle: {
+      fontSize: 14, // Tamanho maior para o título do feedback
+      fontWeight: "bold",
+      color: "#2980b9",
+      marginBottom: 10,
+    },
+    separator: {
+      borderTop: "1px solid #ddd",
+      margin: "8px 0", // Ajustando o espaço ao redor da separação
+    },
+  });
 
-    // definindo o tamanho e cor da fonte para as informações do usuário
-    doc.setFontSize(16);
-    doc.setTextColor(0, 0, 0);
-    doc.text(`Nome: ${name} ${surname}`, margin.left + 10, margin.top + 30);
-    doc.text(`Nível de Liderança: ${leadershipLevel}`, margin.left + 10, margin.top + 40);
-    doc.text(`Pontuação: ${score}`, margin.left + 10, margin.top + 50);
+  return (
+    <Document>
+      <Page style={styles.page}>
+        {/* Título */}
+        <Text style={styles.title}>Resultados do Teste Pro Lidera Skills</Text>
 
-    // desenhando uma linha horizontal para separar as seções
-    doc.line(margin.left, margin.top + 55, 200, margin.top + 55);
+        {/* Introdução */}
+        <View style={styles.section}>
+          <Text style={styles.text}>
+            Parabéns, {name}! Você concluiu o teste!
+            {"\n\n"}
+            Você acaba de dar um passo importante para compreender melhor seu potencial de liderança. Sua dedicação e sinceridade ao responder são fundamentais para obter resultados precisos e valiosos.
+            {"\n\n"}
+            Lembre-se, a liderança não é um destino, mas um processo contínuo de aprendizado e evolução. Use essas informações para crescer e liderar com confiança.
+          </Text>
+        </View>
 
-    // definindo o texto de feedback com a recomendação
-    let feedbackText = "";
-    if (score >= 18 && score <= 35) {
-      feedbackText = "Liderança frágil e pouco trabalhada. Continue buscando desenvolvimento!";
-    } else if (score >= 36 && score <= 53) {
-      feedbackText = "Liderança em desenvolvimento. Invista em mais conteúdos de liderança!";
-    } else if (score >= 54 && score <= 72) {
-      feedbackText = "Líder de alta performance! Continue aprimorando suas habilidades!";
-    }
+        {/* Tabela */}
+        <View style={styles.table}>
+          <View style={[styles.tableRow, { borderTop: "none" }]}>
+            <Text style={[styles.tableCell, styles.tableHeader]}>Campo</Text>
+            <Text style={[styles.tableCell, styles.tableHeader]}>Valor</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCell}>Nível de Liderança</Text>
+            <Text style={styles.tableCell}>{leadershipLevel}</Text>
+          </View>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableCell}>Pontuação</Text>
+            <Text style={styles.tableCell}>{score}</Text>
+          </View>
+        </View>
 
-    doc.setFontSize(12);
-    doc.text(feedbackText, margin.left + 10, margin.top + 65, { maxWidth: 200 - margin.left - margin.right });
+        {/* Feedback */}
+        <View style={styles.feedbackSection}>
+          <Text style={styles.feedbackTitle}>Feedback:</Text>
+          <Text style={styles.feedbackText}>
+            {feedbackText.split("\n").map((line, index) => (
+              <Text key={index}>{line}{"\n"}</Text>
+            ))}
+          </Text>
+        </View>
 
-    // adicionando uma tabela com o nível de liderança e a pontuação
-    doc.autoTable({
-      startY: margin.top + 85,  // define a posição inicial para a tabela
-      head: [["Campo", "Valor"]],  // cabeçalho da tabela
-      body: [
-        ["Nível de Liderança", leadershipLevel],  // dados da tabela
-        ["Pontuação", score.toString()],
-      ],
-      theme: "striped",  // define o estilo de tabela listrado
-      margin: { top: 10, left: 10, right: 10 },  // define as margens da tabela
-    });
+        <View style={styles.separator} />
+      </Page>
+    </Document>
+  );
+};
 
-    // converte o documento gerado para um blob (objeto binário grande)
-    const pdfBlob = doc.output("blob");
+export const PDFButton: React.FC<PDFButtonProps> = ({ score }) => {
+  const name = localStorage.getItem("name") || "Nome não encontrado"; // Busca o nome completo no localStorage
 
-    // cria uma url para o blob do pdf e abre o arquivo em uma nova aba do navegador
-    const url = URL.createObjectURL(pdfBlob);
+  const handlePrint = async () => {
+    const blob = await pdf(<PDFDocument score={score} name={name} />).toBlob();
+    const url = URL.createObjectURL(blob);
     window.open(url, "_blank");
   };
 
-  // retorna o componente Button que ao ser clicado executa a função handlePrint
   return (
     <Button
-    type="button"
-      buttonText="Imprimir Resultado"  // texto do botão
-      onClick={handlePrint}  // ação a ser executada quando o botão for clicado
-      disabled={false}  // define o botão como habilitado
+      type="button"
+      buttonText="Imprimir Resultado"
+      onClick={handlePrint}
     />
   );
 };
--- src/app/components/feedbackMessage.tsx
+++ src/app/components/feedbackMessage.tsx
@@ -1,7 +1,7 @@
-import Result from "../json/results.json";
+import Results from "../json/results.json";
 
 export default function FeedbackMessage({ index }: { index: number }) {
-  const text: string = Result[index].text; // obtém o texto do feedback com base no índice
+  const text: string = Results[index].text; // obtém o texto do feedback com base no índice
 
   return (
     <div className="bg-question dark:bg-questiond p-4 rounded-lg">
