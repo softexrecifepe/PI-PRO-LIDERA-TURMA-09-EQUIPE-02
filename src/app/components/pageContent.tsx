b71781169a2a4ad20b93e7a4a71411849ec712cb
Luana Calisto <luanavenancio060@gmail.com>
2024-11-27 01:09:32 -0300

    centralize page content

--- src/app/components/pageContent.tsx
+++ src/app/components/pageContent.tsx
@@ -1,6 +1,6 @@
 export function PageContent({imageSrc, textContent, titleText}: {imageSrc: string, textContent: string, titleText: string}){
     return(
-        <div className=" mx-16 my-4 text-box p-2 w-[80%]">
+        <div className=" mx-auto my-4 text-box p-2 w-[80%]">
             <h3 className="text-5xl font-bold flex-col w-[450px]">{titleText}</h3>
             <div className="flex">
                 <img className="w-[500px] h-[400px]" src={imageSrc} alt="" />
